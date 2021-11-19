3d8e4ffcebc70052e07db4e69f267f8f4e7b34e9
Pavanmacha0 <93367673+Pavanmacha0@users.noreply.github.com>
2021-11-19 12:32:21 +0530

    Add files via upload

---
+++ pavan 1912.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int x,y,z;
+	printf("enter a value for x");
+	scanf("%d" ,&y);
+	printf("enter the value for y");
+	scanf(%d ,&y);
+	z=x+y;
+	printf("the sum of the two numbers is z: %d ,z")
+
+return(0);	
+}
